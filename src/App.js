83e16a6809b2ab058de509ad5bcb058830bd548a
Uesli Laska <uesli.laska@gmail.com>
2023-08-19 15:28:22 +0200

    BetaPlan Working on first react app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+     <div className="hero_section">
+      {/* Hero Section */}
+      <h1>Hello Dojo</h1>
+    </div>
+    
+    <div className="todo_list">
+      <li>Learn Web Fundamentals</li>
+      <li>Learn React</li>
+      <li>Learn Express</li>
+      <li>Learn MongoDB</li>
+    </div>
+    </>
+   
+
+    
+  );
+}
+
+export default App;
+
